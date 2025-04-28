e9d50aecbc868db13c4b456a59e155ec825a8d4b
Tim Haselaars <timhaselaars@gmail.com>
2025-04-28 15:31:19 +0200

    feat: Added Get started section

--- src/components/Terminal.jsx
+++ src/components/Terminal.jsx
@@ -13,10 +13,10 @@ const Terminal = ({
     const displayCommand = command || `curl -X GET ${endpoint || "http://localhost:8080t"}`;
 
   return (
-    <div className="w-full mt-6 max-w-xl mx-auto">
-      <div className="coding inverse-toggle px-5 pt-4 shadow-lg text-gray-100 text-sm font-mono subpixel-antialiased 
-                          bg-gray-800 pb-6 pt-4 rounded-lg leading-normal overflow-hidden">
-        <div className="top mb-2 flex justify-between">
+    <div className="w-full max-w-xl mx-auto">
+      <div className="coding inverse-toggle px-5 pt-2 shadow-lg text-gray-100 text-sm font-mono subpixel-antialiased
+                          bg-gray-800 pb-2 rounded-lg leading-normal overflow-hidden">
+        <div className="top mb-0 flex justify-between">
           <div className="flex">
             <div className="h-3 w-3 bg-red-500 rounded-full"></div>
             <div className="ml-2 h-3 w-3 bg-orange-300 rounded-full"></div>
@@ -24,9 +24,9 @@ const Terminal = ({
           </div>
           {showCopyButton && <CopyButton textToCopy={displayCommand} />}
         </div>
-        <div className="mt-4 flex">
+        <div className="mt-1 flex">
           <span className="text-green-400">{prompt}</span>
-          <p className="flex-1 typing items-start pl-2">
+          <p className="flex-1 typing items-start pl-2 break-all">
             {displayCommand}
             <br />
           </p>
