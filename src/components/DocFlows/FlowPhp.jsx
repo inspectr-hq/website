ce8d2115aece62b7a952fed45e7840f9c5229057
Thim <timhaselaars@gmail.com>
2025-09-22 08:35:00 +0200

    Add PHP usage guide

---
+++ src/components/DocFlows/FlowPhp.jsx
@@ -0,0 +1,45 @@
+import React, { useMemo } from 'react';
+import FlowVisualizer from '../Flow/FlowVisualizer.jsx';
+import { buildBaseFlow, buildIngressFlow } from '../Flow/FlowBuilder.jsx';
+
+import clientIcon from '../../assets/icons/postman.svg?react';
+import serviceIcon from '../../assets/icons/php_laravel.svg?react';
+
+export function FlowBase() {
+  const { nodes, edges } = useMemo(
+    () =>
+      buildBaseFlow({
+        start: { label: 'Client', icon: clientIcon },
+        end: { label: 'PHP App', icon: serviceIcon, width: 120 }
+      }),
+    []
+  );
+
+  return (
+    <FlowVisualizer
+      nodeData={nodes}
+      edgeData={edges}
+      style={{ width: '100%', height: '210px' }}
+    />
+  );
+}
+
+export function FlowIngress() {
+  const { nodes, edges } = useMemo(
+    () =>
+      buildIngressFlow({
+        overrides: { group_public: { label: 'php-demo.in-spectr.dev' } },
+        start: { label: 'Client', icon: clientIcon },
+        end: { label: 'PHP App', icon: serviceIcon, width: 120 }
+      }),
+    []
+  );
+
+  return (
+    <FlowVisualizer
+      nodeData={nodes}
+      edgeData={edges}
+      style={{ width: '100%', height: '210px' }}
+    />
+  );
+}
